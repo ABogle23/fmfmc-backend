ba65341cd72d64cdf117a0650f3f1b41348407fb
Edward White <edward.a.white@btinternet.com>
2024-07-15 21:05:04 +0100

    feat(eating): adding clust strat interface.

---
+++ src/main/java/com/icl/fmfmc_backend/service/ClusteringStrategy.java
@@ -0,0 +1,8 @@
+package com.icl.fmfmc_backend.service;
+
+import org.locationtech.jts.geom.Point;
+import java.util.List;
+
+public interface ClusteringStrategy {
+  List<Point> clusterChargers(List<Point> points, int numberOfClusters);
+}
