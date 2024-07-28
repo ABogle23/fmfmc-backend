30dc44fe1d899bb7fdd5a7a6d478009a079190b5
Edward White <edward.a.white@btinternet.com>
2024-07-28 15:55:31 +0100

    clean(errors): NoFoodEstablishmentsInRangeOfChargerException spelling correction

---
+++ src/main/java/com/icl/fmfmc_backend/exception/NoFoodEstablishmentsInRangeOfChargerException.java
@@ -0,0 +1,21 @@
+package com.icl.fmfmc_backend.exception;
+
+public class NoFoodEstablishmentsInRangeOfChargerException extends Exception {
+
+
+    public NoFoodEstablishmentsInRangeOfChargerException() {
+        super("No restaurants within range of charger could be found.");
+    }
+
+    public NoFoodEstablishmentsInRangeOfChargerException(String message) {
+        super(message);
+    }
+
+    public NoFoodEstablishmentsInRangeOfChargerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoFoodEstablishmentsInRangeOfChargerException(Throwable cause) {
+        super(cause);
+    }
+}
