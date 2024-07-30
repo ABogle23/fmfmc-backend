347e5e2fc90944c8409684c071313d3a6d945ff5
Edward White <edward.a.white@btinternet.com>
2024-07-30 20:47:56 +0100

    test(routing): added DirectionsClientManagerTest

---
+++ src/test/java/com/icl/fmfmc_backend/DirectionsClientManagerTest.java
@@ -0,0 +1,88 @@
+package com.icl.fmfmc_backend;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.icl.fmfmc_backend.Integration.DirectionsClient;
+import com.icl.fmfmc_backend.dto.Routing.DirectionsRequest;
+import com.icl.fmfmc_backend.dto.Routing.DirectionsResponse;
+import com.icl.fmfmc_backend.exception.BadRequestException;
+import com.icl.fmfmc_backend.exception.DirectionsClientException;
+import com.icl.fmfmc_backend.exception.ServiceUnavailableException;
+import com.icl.fmfmc_backend.service.DirectionsClientManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@ExtendWith(MockitoExtension.class)
+public class DirectionsClientManagerTest {
+
+  private DirectionsClient osrClient;
+  private DirectionsClient mapboxClient;
+  private DirectionsClientManager directionsClientManager;
+
+  @BeforeEach
+  public void setUp() {
+    osrClient = Mockito.mock(DirectionsClient.class);
+    mapboxClient = Mockito.mock(DirectionsClient.class);
+    directionsClientManager = new DirectionsClientManager(osrClient, mapboxClient);
+  }
+
+  @Test
+  void fetchDirectionsSuccessfully() throws DirectionsClientException {
+    DirectionsRequest request = new DirectionsRequest();
+    DirectionsResponse response = new DirectionsResponse();
+    when(osrClient.getDirections(request)).thenReturn(response);
+
+    DirectionsResponse result = directionsClientManager.getDirections(request);
+
+    assertEquals(response, result);
+    verify(osrClient, times(1)).getDirections(request);
+  }
+
+  @Test
+  void switchToMapboxOnServiceUnavailableException() throws DirectionsClientException {
+    DirectionsRequest request = new DirectionsRequest();
+    when(osrClient.getDirections(request))
+        .thenThrow(new ServiceUnavailableException("Service Unavailable"));
+    DirectionsResponse response = new DirectionsResponse();
+    when(mapboxClient.getDirections(request)).thenReturn(response);
+
+    DirectionsResponse result = directionsClientManager.getDirections(request);
+
+    assertEquals(response, result);
+    verify(osrClient, times(1)).getDirections(request);
+    verify(mapboxClient, times(1)).getDirections(request);
+  }
+
+  @Test
+  void throwDirectionsClientExceptionOnBadRequest() {
+    DirectionsRequest request = new DirectionsRequest();
+    when(osrClient.getDirections(request)).thenThrow(new BadRequestException("Bad Request"));
+
+    assertThrows(
+        DirectionsClientException.class, () -> directionsClientManager.getDirections(request));
+    verify(osrClient, times(1)).getDirections(request);
+  }
+
+  @Test
+  void throwRuntimeExceptionOnGenericException() {
+    DirectionsRequest request = new DirectionsRequest();
+    when(osrClient.getDirections(request)).thenThrow(new RuntimeException("Runtime Exception"));
+
+    assertThrows(RuntimeException.class, () -> directionsClientManager.getDirections(request));
+    verify(osrClient, times(1)).getDirections(request);
+  }
+
+  @Test
+  void invalidClientNameDoesNotSwitch() {
+    directionsClientManager.setClient("invalid");
+
+    assertEquals("osr", directionsClientManager.getActiveClientName());
+  }
+}
