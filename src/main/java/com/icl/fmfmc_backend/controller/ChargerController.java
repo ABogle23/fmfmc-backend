5f7cfc3b8cdb03615c36de07e95ddac53f15f220
Edward White <edward.a.white@btinternet.com>
2024-08-10 20:22:21 +0100

    feat(charger-data): getAllChargersWithinBoundingBox method now including filtering chargers by params

--- src/main/java/com/icl/fmfmc_backend/controller/ChargerController.java
+++ src/main/java/com/icl/fmfmc_backend/controller/ChargerController.java
@@ -1,22 +1,27 @@
 package com.icl.fmfmc_backend.controller;
 
+import com.icl.fmfmc_backend.dto.api.ChargerRequest;
+import com.icl.fmfmc_backend.dto.api.RouteRequest;
 import com.icl.fmfmc_backend.dto.charger.ChargerQuery;
 import com.icl.fmfmc_backend.entity.charger.Charger;
 import com.icl.fmfmc_backend.service.ChargerService;
 import com.icl.fmfmc_backend.service.FoodEstablishmentService;
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chargers")
+@RequestMapping("/api/charger")
 @RequiredArgsConstructor
 @Validated
-@Hidden
 public class ChargerController {
   private final ChargerService chargerService;
   private final FoodEstablishmentService foodEstablishmentService;
@@ -27,6 +32,7 @@ public class ChargerController {
    *
    * @return A String
    */
+  @Hidden
   @GetMapping("/sayhello")
   public String sayHelloWorld() {
     return "Hello World!";
@@ -38,7 +44,7 @@ public class ChargerController {
    *
    * @return List of all Chargers in the charger table
    */
-  @GetMapping("/")
+  @GetMapping("/all")
   public ResponseEntity<List<Charger>> getAllChargers() {
     return ResponseEntity.ok().body(chargerService.getAllChargers());
   }
@@ -55,20 +61,36 @@ public class ChargerController {
     return ResponseEntity.ok().body(chargerService.getChargerById(id));
   }
 
-  //TODO: allow for filtering by additional params beyond bounding box
-  @GetMapping("/all-chargers")
+  @Operation(
+      summary = "Get chargers within a bounding box",
+      description =
+          "Returns a list of chargers within a given bounding box and additional parameters",
+      requestBody =
+          @io.swagger.v3.oas.annotations.parameters.RequestBody(
+              description = "Charger request object containing parameters",
+              required = true,
+              content =
+                  @Content(
+                      mediaType = "application/json",
+                      schema = @Schema(implementation = ChargerRequest.class))))
+  @PostMapping("/chargers")
   public ResponseEntity<List<Charger>> getAllChargersWithinBoundingBox(
-      @RequestParam(required = false) Double topLeftLat,
-      @RequestParam(required = false) Double topLeftLong,
-      @RequestParam(required = false) Double bottomRightLat,
-      @RequestParam(required = false) Double bottomRightLong) {
+      @Valid @RequestBody ChargerRequest chargerRequest) {
+
     ChargerQuery chargerQuery =
         ChargerQuery.builder()
-            .topLeftLatLng(topLeftLat, topLeftLong)
-            .bottomRightLatLng(bottomRightLat, bottomRightLong)
+            .topLeftLatLng(chargerRequest.getTopLeftLat(), chargerRequest.getTopLeftLong())
+            .bottomRightLatLng(
+                chargerRequest.getBottomRightLat(), chargerRequest.getBottomRightLong())
+            .minKwChargeSpeed(chargerRequest.getMinKwChargeSpeed())
+            .maxKwChargeSpeed(chargerRequest.getMaxKwChargeSpeed())
+            .accessTypeIds(chargerRequest.getAccessTypes())
+            .connectionTypeIds(chargerRequest.getConnectionTypes())
+            .minNoChargePoints(chargerRequest.getMinNoChargePoints())
             .build();
 
-    return ResponseEntity.ok().body(chargerService.getChargersByParams(chargerQuery));
+    List<Charger> result = chargerService.getChargersByParams(chargerQuery);
 
+    return ResponseEntity.ok().body(result);
   }
 }
