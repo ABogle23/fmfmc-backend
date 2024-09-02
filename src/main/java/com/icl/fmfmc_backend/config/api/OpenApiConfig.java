cce82694eaba9e51f3c19919b76e7ad75648da1e
Edward White <edward.a.white@btinternet.com>
2024-09-02 15:03:23 +0100

    docs(docs config): adding javadoc comments

--- src/main/java/com/icl/fmfmc_backend/config/api/OpenApiConfig.java
+++ src/main/java/com/icl/fmfmc_backend/config/api/OpenApiConfig.java
@@ -5,15 +5,22 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** Configuration class for OpenAPI documentation. */
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Feed me Feed my Car API")
-                        .version("1.0")
-                        .description("API documentation for Feed me Feed my Car."));
-    }
+  /**
+   * Bean definition for customizing the OpenAPI documentation.
+   *
+   * @return an OpenAPI object with custom information
+   */
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Feed me Feed my Car API")
+                .version("1.0")
+                .description("API documentation for Feed me Feed my Car."));
+  }
 }
