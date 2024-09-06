532376e2705aaa41f373c0f3035c86fdbe83788c
Edward White <edward.a.white@btinternet.com>
2024-09-06 20:06:52 +0100

    refactor(polyline): simplifying utility to use google library

--- src/main/java/com/icl/fmfmc_backend/Routing/PolylineUtility.java
+++ src/main/java/com/icl/fmfmc_backend/Routing/PolylineUtility.java
@@ -1,57 +1,19 @@
 package com.icl.fmfmc_backend.Routing;
 
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.LatLng;
 import com.icl.fmfmc_backend.entity.GeoCoordinates;
 import com.icl.fmfmc_backend.service.CoordinateFormatter;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.Polygon;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PolylineUtility {
-
-  private static final double PRECISION = 1E5;
-
-  /**
-   * Encodes a list of GeoCoordinates to a polyline string.
-   *
-   * @param geoCoordinatesList the list of GeoCoordinates to encode
-   * @return the encoded polyline string
-   */
-  public static String encodeGeoCoordinatesToPolyline(List<GeoCoordinates> geoCoordinatesList) {
-    List<List<Double>> coordinates = geoCoordinatesList.stream()
-            .map(gc -> List.of(gc.getLongitude(), gc.getLatitude()))
-            .collect(Collectors.toList());
-    return encodePolyline(coordinates);
-  }
-  /**
-   * Encodes a list of coordinates to a polyline string.
-   *
-   * @param coordinates the list of coordinates to encode
-   * @return the encoded polyline string
-   */
-  public static String encodePolyline(List<List<Double>> coordinates) {
-    StringBuilder encodedPath = new StringBuilder();
-    int prevLat = 0, prevLng = 0;
-
-    for (List<Double> coordinate : coordinates) {
-      int lat = (int) Math.round(coordinate.get(1) * PRECISION);
-      int lng = (int) Math.round(coordinate.get(0) * PRECISION);
-
-      int dLat = lat - prevLat;
-      int dLng = lng - prevLng;
-
-      encodedPath.append(encode(dLat));
-      encodedPath.append(encode(dLng));
-
-      prevLat = lat;
-      prevLng = lng;
-    }
-
-    return encodedPath.toString();
-  }
 
   /**
    * Encodes a LineString to a polyline string.
@@ -60,11 +22,15 @@ public class PolylineUtility {
    * @return the encoded polyline string
    */
   public static String encodeLineString(LineString lineString) {
-    List<List<Double>> coordinates = Arrays.stream(lineString.getCoordinates())
-            .map(coord -> List.of(coord.x, coord.y))
-            .collect(Collectors.toList());
-    return encodePolyline(coordinates);
+    List<LatLng> path = new ArrayList<>();
+
+    for (Coordinate coord : lineString.getCoordinates()) {
+      path.add(new LatLng(coord.y, coord.x)); // JTS uses long lat, google uses lat long
+    }
+
+    return PolylineEncoding.encode(path);
   }
+
   /**
    * Encodes a Polygon to a polyline string.
    *
@@ -72,57 +38,18 @@ public class PolylineUtility {
    * @return the encoded polyline string
    */
   public static String encodePolygon(Polygon polygon) {
-    LineString exteriorRing = polygon.getExteriorRing();
-    return encodeLineString(exteriorRing);
-  }
-  /**
-   * Encodes an integer value to a polyline string.
-   *
-   * @param value the integer value to encode
-   * @return the encoded polyline string
-   */
-  private static String encode(int value) {
-    StringBuilder encoded = new StringBuilder();
-    value = value < 0 ? ~(value << 1) : value << 1;
-    while (value >= 0x20) {
-      encoded.append(Character.toChars((0x20 | (value & 0x1f)) + 63));
-      value >>= 5;
-    }
-    encoded.append(Character.toChars(value + 63));
-    return encoded.toString();
-  }
-
-  @Deprecated
-  public static String polygonStringToFoursquareFormat(Polygon polygon) {
-
-    int step = 75;
+    List<LatLng> path = new ArrayList<>();
     Coordinate[] coordinates = polygon.getExteriorRing().getCoordinates();
-    StringBuilder formattedString = new StringBuilder();
 
-    // iterate over coordinates skipping {step} coordinates each time
-    for (int i = 0; i < coordinates.length; i += step) {
-      // use CoordinateFormatter to trim lat long to 3dp
-      formattedString
-              .append(CoordinateFormatter.formatCoordinate(coordinates[i].y))
-              .append(",")
-              .append(CoordinateFormatter.formatCoordinate(coordinates[i].x));
-
-      // append '~' separator except after last coordinate
-      if (i + step < coordinates.length) {
-        formattedString.append("~");
-      }
+    for (Coordinate coord : coordinates) {
+      path.add(new LatLng(coord.y, coord.x)); // JTS uses long lat, google uses lat long
     }
 
-    // ensure the polygon is closed (first and last coordinates should be the same)
-    if (!coordinates[0].equals2D(coordinates[coordinates.length - 1])) {
-      formattedString
-              .append("~")
-              .append(CoordinateFormatter.formatCoordinate(coordinates[0].y))
-              .append(",")
-              .append(CoordinateFormatter.formatCoordinate(coordinates[0].x));
+    if (!path.get(0).equals(path.get(path.size() - 1))) {
+      path.add(path.get(0));
     }
 
-    return formattedString.toString();
+    return PolylineEncoding.encode(path);
   }
 
 }
